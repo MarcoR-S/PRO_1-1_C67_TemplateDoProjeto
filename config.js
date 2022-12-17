175c5bb90709392f12941b49b6c4fcca559db7ef
MarcoR-S <95389087+MarcoR-S@users.noreply.github.com>
2022-12-17 12:43:24 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,18 @@
 import firebase from 'firebase';
 
-// adicione SDK do Firebase
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD8nIRaRf6fMrnwF_H0f1RPDjBaTGL9dSo",
+  authDomain: "votacao-eb334.firebaseapp.com",
+  projectId: "votacao-eb334",
+  storageBucket: "votacao-eb334.appspot.com",
+  messagingSenderId: "896118972256",
+  appId: "1:896118972256:web:fe7d97d8bc0c5ab7e30805"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 var firebaseConfig = {
 
 };
